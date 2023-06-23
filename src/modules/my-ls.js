5d8d0a46baca36121b0d1db7ac440f60b1fb4379
Aleksandr <ledich19@gmail.com>
2023-06-23 15:51:54 +0300

    refactor: add constants

--- src/modules/my-ls.js
+++ src/modules/my-ls.js
@@ -1,28 +1,26 @@
 import { access, readdir } from "node:fs/promises";
+import { OPERATION_FAILED } from "./constants";
 
 const ls = async (workDirectory) => {
   const tableDir = [];
   const tableFile = [];
-  try {
-    await access(workDirectory);
-    const files = await readdir(workDirectory, { withFileTypes: true });
-    for (const file of files) {
-      if (file.isDirectory()) {
-        tableDir.push({
-          name: file.name,
-          type: "dir",
-        });
-      }
-      if (file.isFile()) {
-        tableFile.push({
-          name: file.name,
-          type: "file",
-        });
-      }
+
+  const files = await readdir(workDirectory, { withFileTypes: true });
+  for (const file of files) {
+    if (file.isDirectory()) {
+      tableDir.push({
+        name: file.name,
+        type: "dir",
+      });
     }
-  } catch (err) {
-    throw Error(`\u001B[31mFS operation failed\u001B[0m ${err.message}`);
+    if (file.isFile()) {
+      tableFile.push({
+        name: file.name,
+        type: "file",
+      });
+    }
   }
+
   console.table([
     ...tableDir.sort((a, b) => a.name - b.name),
     ...tableFile.sort((a, b) => a.name - b.name),
